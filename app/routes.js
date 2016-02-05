b7557442fbb03899551f1a84471a73dba6989dec
matthew-shaw <matthew.shaw@digital.landregistry.gov.uk>
2016-02-05 13:10:41 +0000

    read data from json files, draft register page

--- app/routes.js
+++ app/routes.js
@@ -1,198 +1,186 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
 
-////////////
-// examples
-////////////
+// Beta v2
+
+router.get('/:version/applications', function (req, res) {
+  var version =  req.params.version;
+  res.render(version + '/applications', { 'version': version, 'done': 2, 'total': 8 });
+});
+
+router.get('/:version/applications/:application/draft', function (req, res) {
+  var version = req.params.version;
+  var application = req.params.application;
+  var data = fs.readFileSync(__dirname + '/views/' + version + '/data/' + application + '.json');
+  var parsedData = JSON.parse(data);
+  res.render(version + '/draft_register', { "data": parsedData, "application": application, "version": version });
+});
+
+// beta stuff
+// beta v1
+// No catchers
+
+router.get('/:version/a000101-draft-register', function(req, res, next) {
+  var version =  req.params.version;
+  var answer = req.query.radioindentgroup;
+  console.log(answer);
+  if (answer === 'No') {
+    res.redirect('/' + version + '/a000101-completed-old-world', { 'version' : version });
+  } else {
+    next();
+  }
+});
+
+router.get('/:version/a000101-search-markoff', function(req, res, next) {
+  var version =  req.params.version;
+  var answer = req.query.radioindentgroup;
+  console.log(answer);
+  if (answer === 'No') {
+    res.redirect('/' + version + '/a000101-completed-old-world', { 'version' : version });
+  } else {
+    next();
+  }
+});
 
 
-// router.get('/', function (req, res) {
-//   res.render('index');
+router.get('/:version/a000101-completed', function(req, res, next) {
+  var version =  req.params.version;
+  var answer = req.query.radioindentgroup;
+  console.log(answer);
+  if (answer === 'No') {
+    res.redirect('/' + version + '/a000101-completed-old-world-reg-done', { 'version' : version });
+  } else {
+    next();
+  }
+});
+
+//   app.get('/examples/step1name', function (req, res) {
+//   res.render('examples/step1', { 'name' : 'Bob' });
 // });
 
-// router.get('/examples/template-data', function (req, res) {
-//   res.render('examples/template-data', { 'name' : 'Foo' });
+// app.get('/examples/step2', function (req, res) {
+//   if (req.query.knowsni == 'No') {
+//   res.render('examples/step2',
+//       {   'name' : req.query.fullname,
+//         'age' : req.query.age,
+//         'feeling' : req.query.feeling,
+//         'knowsni' : 'doesnt know ni'
+//       });
+//   } else {
+//       res.render('examples/step2',
+//       {   'name' : req.query.fullname,
+//         'age' : req.query.age,
+//         'feeling' : req.query.feeling
+//       });
+//   };
 // });
 
 
 
 
-  router.get('/:version/applications', function (req, res) {
-    var version =  req.params.version;
-    res.render(version + '/applications', { 'version': version, 'done': 2, 'total': 8 });
-  });
+// old stuff
 
-  router.get('/:version/applications/:application', function (req, res) {
-    var version = req.params.version;
-    var application = req.params.application;
+router.get('/', function (req, res) {
+  res.render('index');
+});
 
-    res.render(version + '/application', { 'version': version });
-  });
+router.get('/examples/template-data', function (req, res) {
+  res.render('examples/template-data', { 'name' : 'Foo' });
+});
 
-  // beta stuff
-  // beta v1
-  // No catchers
+// add your routes here
+router.get('/index-MS', function (req, res) {
+  res.render('index-MS', { 'title' : 'Material Prototype Kit' });
+});
 
-  router.get('/:version/a000101-draft-register', function(req, res, next) {
-    var version =  req.params.version;
-    var answer = req.query.radioindentgroup;
-    console.log(answer);
-    if (answer === 'No') {
-      res.redirect('/' + version + '/a000101-completed-old-world', { 'version' : version });
-    } else {
-      next();
-    }
-  });
+//Version 1
+router.get('/MS-v1/cases1', function (req, res) {
+  res.render('MS-v1/cases1', { 'title' : 'Cases' });
+});
 
-  router.get('/:version/a000101-search-markoff', function(req, res, next) {
-    var version =  req.params.version;
-    var answer = req.query.radioindentgroup;
-    console.log(answer);
-    if (answer === 'No') {
-      res.redirect('/' + version + '/a000101-completed-old-world', { 'version' : version });
-    } else {
-      next();
-    }
-  });
+router.get('/MS-v1/case1', function (req, res) {
+  res.render('MS-v1/case1', { 'title' : 'Case', 'ref' : 'REF0782', 'type' : 'Mortgage' });
+});
 
+router.get('/MS-v1/cases2', function (req, res) {
+  res.render('MS-v1/cases2', { 'title' : 'Cases' });
+});
 
-  router.get('/:version/a000101-completed', function(req, res, next) {
-    var version =  req.params.version;
-    var answer = req.query.radioindentgroup;
-    console.log(answer);
-    if (answer === 'No') {
-      res.redirect('/' + version + '/a000101-completed-old-world-reg-done', { 'version' : version });
-    } else {
-      next();
-    }
-  });
+router.get('/MS-v1/case2', function (req, res) {
+  res.render('MS-v1/case2', { 'title' : 'Case', 'ref' : 'REF0782', 'type' : 'Mortgage' });
+});
 
-    //   app.get('/examples/step1name', function (req, res) {
-    //   res.render('examples/step1', { 'name' : 'Bob' });
-    // });
+router.get('/MS-v1/task', function (req, res) {
+  res.render('MS-v1/task', { 'title' : 'Task' });
+});
 
-    // app.get('/examples/step2', function (req, res) {
-    //   if (req.query.knowsni == 'No') {
-    //   res.render('examples/step2',
-    //       {   'name' : req.query.fullname,
-    //         'age' : req.query.age,
-    //         'feeling' : req.query.feeling,
-    //         'knowsni' : 'doesnt know ni'
-    //       });
-    //   } else {
-    //       res.render('examples/step2',
-    //       {   'name' : req.query.fullname,
-    //         'age' : req.query.age,
-    //         'feeling' : req.query.feeling
-    //       });
-    //   };
-    // });
+//Version 2
+router.get('/MS-v2/cases1', function (req, res) {
+  res.render('MS-v2/cases1', { 'title' : 'Cases' });
+});
+
+router.get('/MS-v2/case1', function (req, res) {
+  res.render('MS-v2/case1', { 'title' : 'Case', 'ref' : 'REF0782', 'type' : 'Mortgage' });
+});
+
+router.get('/MS-v2/cases2', function (req, res) {
+  res.render('MS-v2/cases2', { 'title' : 'Cases' });
+});
+
+router.get('/MS-v2/case2', function (req, res) {
+  res.render('MS-v2/case2', { 'title' : 'Case', 'ref' : 'REF0782', 'type' : 'Mortgage' });
+});
+
+router.get('/MS-v2/task1', function (req, res) {
+  res.render('MS-v2/task1', { 'title' : 'Task' });
+});
+
+router.get('/MS-v2/task2', function (req, res) {
+  res.render('MS-v2/task2', { 'title' : 'Task' });
+});
+
+router.get('/MS-v2/case3', function (req, res) {
+  res.render('MS-v2/case3', { 'title' : 'Case', 'ref' : 'REF0782', 'type' : 'Mortgage' });
+});
+
+router.get('/MS-v2/case4', function (req, res) {
+  res.render('MS-v2/case4', { 'title' : 'Case', 'ref' : 'REF0782', 'type' : 'Mortgage' });
+});
 
 
+//Version 3
+router.get('/MS-v3/cases1', function (req, res) {
+  res.render('MS-v3/cases1', { 'title' : 'Cases' });
+});
 
+router.get('/MS-v3/case1', function (req, res) {
+  res.render('MS-v3/case1', { 'title' : 'Case', 'ref' : 'REF0782', 'type' : 'Mortgage' });
+});
 
-  // old stuff
+router.get('/MS-v3/cases2', function (req, res) {
+  res.render('MS-v3/cases2', { 'title' : 'Cases' });
+});
 
-  router.get('/', function (req, res) {
-    res.render('index');
-  });
+router.get('/MS-v3/case2', function (req, res) {
+  res.render('MS-v3/case2', { 'title' : 'Case', 'ref' : 'REF0782', 'type' : 'Mortgage' });
+});
 
-  router.get('/examples/template-data', function (req, res) {
-    res.render('examples/template-data', { 'name' : 'Foo' });
-  });
+router.get('/MS-v3/task1', function (req, res) {
+  res.render('MS-v3/task1', { 'title' : 'Task' });
+});
 
-  // add your routes here
-  router.get('/index-MS', function (req, res) {
-    res.render('index-MS', { 'title' : 'Material Prototype Kit' });
-  });
+router.get('/MS-v3/task2', function (req, res) {
+  res.render('MS-v3/task2', { 'title' : 'Task' });
+});
 
-  //Version 1
-  router.get('/MS-v1/cases1', function (req, res) {
-    res.render('MS-v1/cases1', { 'title' : 'Cases' });
-  });
+router.get('/MS-v3/case3', function (req, res) {
+  res.render('MS-v3/case3', { 'title' : 'Case', 'ref' : 'REF0782', 'type' : 'Mortgage' });
+});
 
-  router.get('/MS-v1/case1', function (req, res) {
-    res.render('MS-v1/case1', { 'title' : 'Case', 'ref' : 'REF0782', 'type' : 'Mortgage' });
-  });
-
-  router.get('/MS-v1/cases2', function (req, res) {
-    res.render('MS-v1/cases2', { 'title' : 'Cases' });
-  });
-
-  router.get('/MS-v1/case2', function (req, res) {
-    res.render('MS-v1/case2', { 'title' : 'Case', 'ref' : 'REF0782', 'type' : 'Mortgage' });
-  });
-
-  router.get('/MS-v1/task', function (req, res) {
-    res.render('MS-v1/task', { 'title' : 'Task' });
-  });
-
-  //Version 2
-  router.get('/MS-v2/cases1', function (req, res) {
-    res.render('MS-v2/cases1', { 'title' : 'Cases' });
-  });
-
-  router.get('/MS-v2/case1', function (req, res) {
-    res.render('MS-v2/case1', { 'title' : 'Case', 'ref' : 'REF0782', 'type' : 'Mortgage' });
-  });
-
-  router.get('/MS-v2/cases2', function (req, res) {
-    res.render('MS-v2/cases2', { 'title' : 'Cases' });
-  });
-
-  router.get('/MS-v2/case2', function (req, res) {
-    res.render('MS-v2/case2', { 'title' : 'Case', 'ref' : 'REF0782', 'type' : 'Mortgage' });
-  });
-
-  router.get('/MS-v2/task1', function (req, res) {
-    res.render('MS-v2/task1', { 'title' : 'Task' });
-  });
-
-  router.get('/MS-v2/task2', function (req, res) {
-    res.render('MS-v2/task2', { 'title' : 'Task' });
-  });
-
-  router.get('/MS-v2/case3', function (req, res) {
-    res.render('MS-v2/case3', { 'title' : 'Case', 'ref' : 'REF0782', 'type' : 'Mortgage' });
-  });
-
-  router.get('/MS-v2/case4', function (req, res) {
-    res.render('MS-v2/case4', { 'title' : 'Case', 'ref' : 'REF0782', 'type' : 'Mortgage' });
-  });
-
-
-  //Version 3
-  router.get('/MS-v3/cases1', function (req, res) {
-    res.render('MS-v3/cases1', { 'title' : 'Cases' });
-  });
-
-  router.get('/MS-v3/case1', function (req, res) {
-    res.render('MS-v3/case1', { 'title' : 'Case', 'ref' : 'REF0782', 'type' : 'Mortgage' });
-  });
-
-  router.get('/MS-v3/cases2', function (req, res) {
-    res.render('MS-v3/cases2', { 'title' : 'Cases' });
-  });
-
-  router.get('/MS-v3/case2', function (req, res) {
-    res.render('MS-v3/case2', { 'title' : 'Case', 'ref' : 'REF0782', 'type' : 'Mortgage' });
-  });
-
-  router.get('/MS-v3/task1', function (req, res) {
-    res.render('MS-v3/task1', { 'title' : 'Task' });
-  });
-
-  router.get('/MS-v3/task2', function (req, res) {
-    res.render('MS-v3/task2', { 'title' : 'Task' });
-  });
-
-  router.get('/MS-v3/case3', function (req, res) {
-    res.render('MS-v3/case3', { 'title' : 'Case', 'ref' : 'REF0782', 'type' : 'Mortgage' });
-  });
-
-  router.get('/MS-v3/case4', function (req, res) {
-    res.render('MS-v3/case4', { 'title' : 'Case', 'ref' : 'REF0782', 'type' : 'Mortgage' });
-  });
+router.get('/MS-v3/case4', function (req, res) {
+  res.render('MS-v3/case4', { 'title' : 'Case', 'ref' : 'REF0782', 'type' : 'Mortgage' });
+});
 
 
 
